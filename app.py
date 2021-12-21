b231fb70555abd8efd6f4725e132c72f0c808ea4
Nate Baker <nathantbaker@gmail.com>
2021-12-20 21:49:00 -0600

    Update app.py

--- app.py
+++ app.py
@@ -136,7 +136,7 @@ def webhook():
             return "ok", 200
         
         elif '/HELP' in userText.upper() or '/COMMANDS' in userText.upper() or '/LIST' in userText.upper():
-             send_message('help text will go here!\\ncool') #TODO
+             send_message("Beep Boop. Here is what I can do!\\n\\n##############################################\\n\\n\/8ball ......... Magic Eight Ball response\\n\/clue .......... Discover whodunit!\\n\/finger /fuck .. Curated middle finger\\n\/fortune ....... See someone's fortune\\n\/help .......... See this screen\\n\/hi ............ Say hi to Village Bot\\n\/ryanjoke ...... Get ready to eye roll...\\n\/start ......... Activate Village Bot\\n\/stop .......... Kill Village Bot\\n\/who ........... Returns a random Villager\\n\/which2 ........ 2 random villager names\\n\/which3 ........ Returns 3 random villagers\\n\\n##############################################\\n")
                 
         elif '/CLUE' in userText.upper() or '/MURDER' in userText.upper():
             villager1 = random.choice(villagers)
